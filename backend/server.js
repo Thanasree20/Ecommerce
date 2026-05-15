523fe1669045ce51b4f7c33fca3613a4c7ac06d7
Thanasree20 <thanasree2006vit@gmail.com>
2026-05-15 14:35:50 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,34 @@ const cors = require("cors")
 
 const app = express()
 
+// Middleware
+
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/auth", require("./routes/authRoutes"))
-app.use("/api/products", require("./routes/productRoutes"))
-app.use("/api/orders", require("./routes/orderRoutes"))
+// Routes
 
-app.listen(5000, () => {
-  console.log("Server Running on 5000")
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+)
+
+app.use(
+  "/api/products",
+  require("./routes/productRoutes")
+)
+
+app.use(
+  "/api/orders",
+  require("./routes/orderRoutes")
+)
+
+// Port
+
+const PORT = process.env.PORT || 5000
+
+// Server
+
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`)
 })
